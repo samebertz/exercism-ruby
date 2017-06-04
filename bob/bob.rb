ba10683c788ea48847d0489fd0214a78e9e0becc
Sam Ebertz <samuel.ebertz@gmail.com>
2017-06-04 00:28:59 -0500

    completed exercise: Bob (iteration 1)

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,4 +1,17 @@
 class Bob
-  def self.hey(str)
+  @@RESPONSES = {
+    blank: "Fine. Be that way!",
+    yell: "Whoa, chill out!",
+    question: "Sure.",
+    default: "Whatever."
+  }
+  class << self
+    def hey(str)
+      return @@RESPONSES[:blank] if str.strip.empty?
+      str.gsub!(/[^\Wa-zA-Z]/,'')
+      return @@RESPONSES[:yell] if str.index(/[a-zA-Z]/) && str.upcase == str
+      return @@RESPONSES[:question] if str.end_with?("?")
+      @@RESPONSES[:default]
+    end
   end
 end
