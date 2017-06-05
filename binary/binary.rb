0de09868e92c129a2ff91d9f0441d85c39e83638
Sam Ebertz <samuel.ebertz@gmail.com>
2017-06-05 12:52:08 -0500

    completed exercise: Binary (iteration 2)

--- binary/binary.rb
+++ binary/binary.rb
@@ -2,10 +2,13 @@ class Binary
   class << self
     def to_decimal(bitstring)
       raise ArgumentError if bitstring.index(/[^01]/)
-      bitstring.chars.reverse.each.with_index.reduce(0) do |n, (b, i)|
-        n += b.to_i*(2**i)
+      bits = bitstring.chars.each { |chr| chr.to_i }
+      decimal = 0
+      bits.reverse.reduce(0) do |power, bit|
+        decimal += bit.to_i * 2**power
+        power += 1
       end
-      # bitstring.to_i(2)
+      decimal
     end
   end
 end
