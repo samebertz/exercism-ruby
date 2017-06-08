525e517fc55137193f3849ace96d8c9c5a82a3f5
Sam Ebertz <samuel.ebertz@gmail.com>
2017-06-08 14:36:12 -0500

    completed exercise: Phone Number (iteration 2)

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -1,15 +1,8 @@
 class PhoneNumber
+  NANP = Regexp.compile(/\A1?([2-9][[:digit:]]{2}[2-9][[:digit:]]{6})\Z/)
   class << self
     def clean(str)
-      str.gsub!(/[^[[:digit:]]]/,"")
-      if (str.size == 10
-            || str.size == 11 && str.start_with?("1") && str = str[1..-1])
-          && (2..9).include?(str[0].to_i)
-          && (2..9).include?(str[3].to_i)
-        str
-      else
-        nil
-      end
+      NANP.match(str.gsub(/[^[[:digit:]]]/,"")) ? $~[1] : $~
     end
   end
 end
