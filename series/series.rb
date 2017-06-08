a19667ae125d81c976068dab31313d80cd6d6a9d
Sam Ebertz <samuel.ebertz@gmail.com>
2017-06-08 13:40:41 -0500

    completed exercise: Series (iteration 1)

--- series/series.rb
+++ series/series.rb
@@ -3,5 +3,11 @@ class Series
     @digits = digits
   end
   def slices(n)
+    raise ArgumentError if n > @digits.length
+    slices = []
+    for i in 0 .. @digits.length - n do
+      slices << @digits[i ... i + n]
+    end
+    slices
   end
 end
