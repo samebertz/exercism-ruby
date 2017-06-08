198db2b01e1f51417963be07fd2b3bb89fd4dcf9
Sam Ebertz <samuel.ebertz@gmail.com>
2017-06-08 13:41:34 -0500

    completed exercise: Series (iteration 2)

--- series/series.rb
+++ series/series.rb
@@ -2,7 +2,7 @@ class Series
   def initialize(digits)
     @digits = digits
   end
-  def slices_(n)
+  def slices(n)
     raise ArgumentError if n > @digits.length
     @digits[0..-n].chars.each.with_index.reduce([]) do |memo, (obj, i)|
       memo << @digits[i...i+n]
